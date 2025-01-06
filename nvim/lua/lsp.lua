53168335026043200fe113f4842d5e5701c97714
Vidar André Bø <vidar.bo@bouvet.no>
2025-01-06 09:08:32 +0100

    ts lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -11,7 +11,7 @@ require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
     ensure_installed = {
-        'tsserver',
+        'eslint',
         'rust_analyzer',
         'gopls',
         'clangd',
@@ -57,6 +57,9 @@ lspconfig.rust_analyzer.setup {
             },
         },
     },
+}
+lspconfig.eslint.setup {
+    capabilities = capabilities
 }
 lspconfig.volar.setup {}
 lspconfig.pyright.setup {}
