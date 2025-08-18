09f950db58179d637c30b78bbff3a4416cdf5b2f
Vidar André Bø <vidarandre.bo@protonmail.com>
2025-08-18 21:38:20 +0200

    blink and treesitter

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {{
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = {"lua_ls"},
+        ensure_installed = {"lua_ls", "gopls"},
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
